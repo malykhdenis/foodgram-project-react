c3cbb56418f9226bc7505df9efe9c6067c6738d8
Малых Денис <malykhdk@ya.ru>
2022-08-08 23:10:12 +0300

    PATCH DELETE api/recepies/{id}/

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -118,3 +118,22 @@ class RecipesSerializer(serializers.ModelSerializer):
                 recipe=recipe, ingredient_id=ingredient.get('id'),
                 amount=ingredient.get('amount'),)
         return recipe
+
+    def update(self, instance, validated_data):
+        """Updating recipe and relations ingredients in recipe."""
+        instance.tags.clear()
+        tags = self.initial_data.get('tags')
+        instance.tags.set(tags)
+        IngredientsInRecipe.objects.filter(recipe=instance).all().delete()
+        ingredients = self.initial_data.get('ingredients')
+        for ingredient in ingredients:
+            IngredientsInRecipe.objects.create(
+                recipe=instance, ingredient_id=ingredient.get('id'),
+                amount=ingredient.get('amount'),)
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time)
+        instance.save()
+        return instance
