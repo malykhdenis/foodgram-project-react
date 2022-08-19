1528c8d76927051b79fe907db9e970df7e8812be
Малых Денис <malykhdk@ya.ru>
2022-08-20 00:25:36 +0300

    DjangoFilterBackend default, recipes ordering date

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -99,6 +99,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+        ordering = ('date',)
 
     def __str__(self):
         return self.name
