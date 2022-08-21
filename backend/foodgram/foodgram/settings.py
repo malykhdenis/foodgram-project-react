75f0d9e6fc2042fa6342d30c83bfce29a446a0ec
Малых Денис <malykhdk@ya.ru>
2022-08-22 01:00:20 +0300

    second deploy rev1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['130.193.42.192',
+ALLOWED_HOSTS = ['51.250.103.200',
                  'malykhdk.sytes.net',
                  '127.0.0.1',
                  'localhost',
